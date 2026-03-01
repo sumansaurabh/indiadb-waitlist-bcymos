17aabe55147ca912c91b0aed008383f39d4bb49d
Sunil Agarwal <sunilagwl5@gmail.com>
2026-03-01 15:54:20 +0530

    Revise ProductHuntEmbed image URL and attributes
    Updated the ProductHuntEmbed component to use a new image URL and adjusted the timestamp parameter.

--- app/components/product-hunt-embed.tsx
+++ app/components/product-hunt-embed.tsx
@@ -2,18 +2,7 @@ import React from 'react';
 
 export function ProductHuntEmbed() {
   return (
-    <a 
-      href="https://www.producthunt.com/products/indbase?embed=true&utm_source=badge-featured&utm_medium=badge&utm_campaign=badge-indbase" 
-      target="_blank" 
-      rel="noopener noreferrer"
-    >
-      <img 
-        alt="Indbase - Database for India's Builders | Product Hunt" 
-        width="250" 
-        height="54" 
-        src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=1087639&theme=light&t=1772353244237" 
-        style={{ width: '250px', height: '54px' }} 
-      />
-    </a>
+    <a href="https://www.producthunt.com/products/indbase?embed=true&amp;utm_source=badge-featured&amp;utm_medium=badge&amp;utm_campaign=badge-indbase" target="_blank" rel="noopener noreferrer"><img alt="Indbase - Database for India's Builders | Product Hunt" width="250" height="54" src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=1087639&amp;theme=light&amp;t=1772359819519"/></a>
   );
+  
 }
